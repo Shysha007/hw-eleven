ce3698770931da5c5bd741d245a11bbb45408257
Ольга <91964782+Shysha007@users.noreply.github.com>
2021-10-16 15:35:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const num = +prompt(`Введите число:`);
 let result = true;
-if (num > 1 && !isNanN(num){
+if (num > 1 && !isNanN(num)){
     for (let i = 2; i < num; i++) {
         if (num % i === 0) {
             result = false;
@@ -9,5 +9,5 @@ if (num > 1 && !isNanN(num){
     }
  alert(result);
 } else {
-    alert(`Число 1 — не является ни простым, ни составным числом, так как у него только один делитель`);
+    alert(`Вы ввели нечисло или же число 1 (не является ни простым, ни составным числом)`);
 } 
